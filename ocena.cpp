b36481f87484bc7588d301defddbba58f836c545
Filip Dabrowski <filip.a.dabrowski@gmail.com>
2025-06-26 13:37:43 +0200

    Fixed Ocena class

--- ocena.cpp
+++ ocena.cpp
@@ -1,3 +1,41 @@
 #include "ocena.h"
 
-Ocena::Ocena() {}
+// implementacja konstruktora
+Ocena::Ocena(int studentId, int przedmiotId, double ocena = 2.0, bool zal = false)
+{
+    this -> studentId = studentId;
+    this -> przedmiotId = przedmiotId;
+    this -> ocena = ocena;
+    zaliczony = zal;
+}
+
+// impementacja metod
+int Ocena::getStudentId() const
+{
+    return studentId;
+}
+
+int Ocena::getPrzedmiotId() const
+{
+    return przedmiotId;
+}
+
+double Ocena::getOcena() const
+{
+    return ocena;
+}
+
+bool Ocena::czyZal() const
+{
+    return zaliczony;
+}
+
+void Ocena::setOcena(double ocena)
+{
+    this -> ocena = ocena;
+}
+
+void Ocena::setZal(bool czyZal)
+{
+    zaliczony = czyZal;
+}
--- ocena.h
+++ ocena.h
@@ -4,7 +4,26 @@
 class Ocena
 {
 public:
-    Ocena();
+    //konstuktor
+    Ocena(int studentId, int przedmiotId, double ocena, bool zal);
+
+    // getters
+    int getStudentId() const;
+    int getPrzedmiotId() const;
+    double getOcena() const;
+    bool czyZal() const;
+
+    // setters
+    void setOcena(double ocena);
+    void setZal(bool czyZal);
+
+private:
+
+    // pola / atrybuty
+    int studentId;
+    int przedmiotId;
+    double ocena;
+    bool zaliczony;
 };
 
 #endif // OCENA_H
