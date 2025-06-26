36aeeb037d8bbaea8da7498ba73ad91ec4580ade
Filip Dabrowski <filip.a.dabrowski@gmail.com>
2025-06-26 09:16:15 +0200

    Added implementation of Przemiot class

--- przedmiot.cpp
+++ przedmiot.cpp
@@ -1,3 +1,26 @@
 #include "przedmiot.h"
 
-Przedmiot::Przedmiot() {}
+// implementacja konstruktora
+Przedmiot::Przedmiot(QString id, QString nazwa)
+{
+    this -> id = id;
+    this -> nazwa = nazwa;
+}
+
+// implementacja metod
+QString Przedmiot::toStringCSV()
+{
+    Qstring nazwaSformatowana = nazwa;
+    nazwaSformatowana.replace(",", "_");    // zastąp "," w celu uniknięcia uszkodzenia struktury pliku csv
+    return id + ", " + nazwaSformatowana;
+}
+
+// optional w celu umożliwienia zwrócenia pustej wartości
+std::optional<Przemiot> Przedmiot::fromStringCSV(QString wiersz)
+{
+    QStringList skladowa = wiersz.split(",", Qt::SkipEmptyParts);
+    if(skladowe.size() < 2){
+        return NULL;
+    }
+    return Przedmiot(skladowa[0].trimmed(), skladowa[1].trimmed);
+}
