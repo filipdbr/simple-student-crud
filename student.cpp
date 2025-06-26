e5bf243ba6f5f38a6b09394f3a57764b2f7ceca3
Filip Dabrowski <filip.a.dabrowski@gmail.com>
2025-06-26 13:38:03 +0200

    Fixed Student class

--- student.cpp
+++ student.cpp
@@ -1,3 +1,35 @@
 #include "student.h"
 
-Student::Student() {}
+// implementacja konstruktora
+Student::Student(int id, QString imie, QString nazwisko)
+{
+    studentId = id;
+    this -> imie = imie.toUpper();
+    this -> nazwisko = nazwisko.toUpper();
+}
+
+// implementacje metod
+int Student::getId() const
+{
+    return studentId;
+}
+
+QString Student::getImie() const
+{
+    return imie;
+}
+
+QString Student::getNazwisko() const
+{
+    return nazwisko;
+}
+
+void Student::setImie(QString imie)
+{
+    this -> imie = imie;
+}
+
+void Student::setNazwisko(QString nazwisko)
+{
+    this -> nazwisko = nazwisko;
+}
